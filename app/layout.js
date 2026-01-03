2dfbb2b176bce92c3d40f52d40ee7205c928f0a9
Gyana Singh <gyan123priya@gmail.com>
2026-01-03 18:14:13 +0530

    hii

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,8 @@
-"use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { useEffect, useState } from "react";
-import useFriendSocket from "./utils/friendsRequestSoket";
-
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata = {
   title: "Chat Admin Panel",
@@ -17,38 +10,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [userId, setUserId] = useState(null);
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const id = localStorage.getItem("userId");
-      const data = localStorage.getItem("userData");
-
-      setUserId(id);
-      if (data) {
-        try {
-          setUserData(JSON.parse(data));
-        } catch (e) {
-          console.error("Failed to parse userData:", e);
-        }
-      }
-    }
-  }, []);
-
-  useFriendSocket(userId, userData?.username, userData?.avatar, {
-    onRequestReceived: (data) => {
-      console.log("🔔 Friend request from:", data.fromUsername);
-    },
-    onRequestAccepted: (data) => {
-      console.log("🎉 Request accepted by:", data.fromUsername);
-    },
-  });
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
