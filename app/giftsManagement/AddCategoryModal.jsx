0b3ea391abe7917b2ad08b1047feb4d10f6680f9
Gyana Singh <gyan123priya@gmail.com>
2026-01-09 01:00:03 +0530

    hii

--- app/giftsManagement/AddCategoryModal.jsx
+++ app/giftsManagement/AddCategoryModal.jsx
@@ -20,7 +20,7 @@ export default function AddCategoryModal({ close, onCategoryAdded }) {
       setLoading(true);
 
       const res = await axios.post(
-        "https://chat-app-1-qvl9.onrender.com/api/category/addCategory",
+        "https://chat-app-1-qvl9.onrender.com/api/gift/addCategory",
         { name: name.trim() },
         { withCredentials: true }
       );
